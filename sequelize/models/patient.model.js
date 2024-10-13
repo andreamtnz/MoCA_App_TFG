9a6e047b73a2906dffcbb3c3bd9307ab6450a4b3
andreamtnz <andrea.martinezpalacios@usp.ceu.es>
2024-10-13 19:09:33 +0200

    modified patient

--- sequelize/models/patient.model.js
+++ sequelize/models/patient.model.js
@@ -1,8 +1,8 @@
 const {DataTypes} = require('sequelize');
 
 module.exports = (sequelize) => {
-    sequelize.define('user', { // nombre de la tabla: USER
-        id: { // asi se pone casa atributo
+    sequelize.define('patient', { 
+        id: { 
             allowNull: false,
             autoIncrement: true,
             primaryKey: true,
